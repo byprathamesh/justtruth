9ccbd2d5d527f20ce75406409162bc7d74c4fa57
byprathamesh <prathameshgavhane3@gmail.com>
2025-04-26 02:45:42 +0530

    Cleanup: Remove unused Index component and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import ScanPage from "./pages/ScanPage";
@@ -31,7 +29,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/original" element={<Index />} />
           <Route path="/scan" element={<ScanPage />} />
           <Route path="/product/:productId" element={<ProductPage />} />
           <Route path="/compare/:comparisonId" element={<ComparisonPage />} />
